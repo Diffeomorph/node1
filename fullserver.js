873122c7864ca172fd318c32caaa496c54d5d1db
gf1721 <gf1721@bristol.ac.uk>
2017-12-28 16:04:01 +0000

    othermethods

--- fullserver.js
+++ fullserver.js
@@ -1,3 +1,5 @@
+import { readdir } from "fs";
+
 var http = require("http"), fs = require("fs");
 
 var methods = Object.create(null);
@@ -40,4 +42,37 @@ methods.GET = function(path,respond){
             respond(200,fs.createReadStream(path),
                     require("mime").lookup(path));
     });
+};
+
+methods.DELETE = function(path, respond){
+    fs.stat(path, function(error,stats){
+        if (error && error.code == "ENOENT")
+            respond(204);
+        else if (error)
+            respond(500, error.toString());
+        else if (stats.isDirectory())
+            fs.rmdir(path, respondErrorOrNothing(respond));
+        else
+            fs.unlink(path, respondErrorOrNothing(respond));
+    });
+};
+
+function respondErrorOrNothing(respond){
+    return function(error){
+        if (error)
+            respond(500,error.toString());
+        else 
+            respond(204);
+    };
+}
+
+methods.PUT = function(path,respond, request){
+    var outStream = fs.createWriteStream(path);
+    outStream.on("error", function(error){
+        respond(500, error.toString());
+    });
+    outStream.on("finish", function(){
+        respond(204);
+    });
+    request.pipe(outStream);
 };
